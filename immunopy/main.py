efb030691c444de3860d27329df6113f94dfd423
Eugene Dvoretsky <radioxoma@gmail.com>
2014-04-03 17:19:01 +0200

    Properties adjustment

--- immunopy/main.py
+++ immunopy/main.py
@@ -7,7 +7,6 @@ Created on 18 Jan. 2014 г.
 @author: radioxoma
 """
 
-import sys
 import time
 from multiprocessing import Pool
 import numpy as np
@@ -23,11 +22,11 @@ import lut
 
 MAGNIFICATION = '10'
 
-THRESHOLD_SHIFT = 10
+THRESHOLD_SHIFT = 20
 PEAK_DISTANCE = 8
-MIN_SIZE = 15
+MIN_SIZE = 10
 MAX_SIZE = 3000
-VTYPE = 0
+VTYPE = 1
 
 
 # DEVICE = ['Camera', 'DemoCamera', 'DCam']
@@ -106,7 +105,7 @@ def process(image, scale, threshold_shift, peak_distance, min_size, max_size):
 #     hemfiltered, hemfnum = hproc.get(timeout=5)
 #     dabfiltered, dabfnum = dproc.get(timeout=5)
     hemfiltered, hemfnum = worker(hem, threshold_shift, peak_distance, min_size, max_size)
-    dabfiltered, dabfnum = worker(dab, threshold_shift, peak_distance, min_size, max_size)
+    dabfiltered, dabfnum = worker(dab, threshold_shift+10, peak_distance, min_size, max_size)
     # MULTICORE END ---------------------------------------------------------
     
     # Stats
@@ -116,17 +115,16 @@ def process(image, scale, threshold_shift, peak_distance, min_size, max_size):
 
     # Visualization
     if VTYPE == 0:
-        overlay = iptools.overlay(scaled, dabfiltered, hemfiltered)
+        overlay = scaled
     elif VTYPE == 1:
+        overlay = iptools.overlay(scaled, dabfiltered, hemfiltered)
+    elif VTYPE == 2:
         overlay = lut.apply_lut(dabfiltered, LUT)
     else:
         overlay = lut.apply_lut(hemfiltered, LUT)
     cv2.putText(overlay, stats, (2,25), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 0), thickness=2)
     cv2.putText(overlay, stats2, (2,55), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 0), thickness=2)
     cv2.putText(overlay, stats3, (2,85), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 0), thickness=2)
-#     cv2.imshow('Video', scaled[...,::-1])
-#     composite_rgb = np.dstack((hemfiltered, np.zeros_like(hemfiltered), dabfiltered)) # NB! BGR
-#     cv2.imshow('Masks', composite_rgb.astype(np.float32))
     return overlay
     
 
@@ -140,7 +138,7 @@ if __name__ == '__main__':
     mmc = MMCorePy.CMMCore()
     print('ImageBufferSize %f' % mmc.getImageBufferSize())  # Returns the size of the internal image buffer.
     print('BufferTotalCapacity %f' % mmc.getBufferTotalCapacity())
-    mmc.setCircularBufferMemoryFootprint(60)
+    mmc.setCircularBufferMemoryFootprint(100)
     mmc.enableStderrLog(False)
     mmc.enableDebugLog(False)
     mmc.loadDevice(*DEVICE)
@@ -148,12 +146,11 @@ if __name__ == '__main__':
     mmc.setCameraDevice(DEVICE[0])
     # mmc.setProperty(DEVICE[0], 'Binning', '2')
     mmc.setProperty(DEVICE[0], 'PixelType', '32bitRGB')
-    iptools.set_mmc_resolution(mmc, 1024, 768)
+    iptools.set_mmc_resolution(mmc, 500, 500)
     mmc.snapImage()  # Baumer bug workaround
     # mmc.initializeCircularBuffer()
-#     cv2.namedWindow('Video')
     cv2.namedWindow('Overlay')
-    cv2.namedWindow('Controls')
+    cv2.namedWindow('Controls', flags=cv2.WINDOW_NORMAL | cv2.WINDOW_AUTOSIZE)
     if mmc.hasProperty(DEVICE[0], 'Gain'):
         cv2.createTrackbar(
             'Gain', 'Controls',
@@ -170,15 +167,14 @@ if __name__ == '__main__':
     cv2.createTrackbar('PEAK_DISTANCE', 'Controls', 8, 100, set_peak_distance)
     cv2.createTrackbar('MAX_SIZE', 'Controls', MAX_SIZE, 5000, set_max_size)
     cv2.createTrackbar('MIN_SIZE', 'Controls', MIN_SIZE, 1000, set_min_size)
-    cv2.createTrackbar('VMethod', 'Controls', 0, 2, set_vtype)
+    cv2.createTrackbar('VMethod', 'Controls', VTYPE, 3, set_vtype)
     
     POOL = Pool(processes=2)
-#   FPS single thread 1.35 > 1.8
     mmc.startContinuousSequenceAcquisition(1)
     while True:
         start_time = time.time()
-        rgb32 = mmc.getLastImage()
-        if rgb32 is not None:
+        if mmc.getRemainingImageCount() > 0:
+            rgb32 = mmc.getLastImage()
             cv2.imshow('Overlay', process(
                 iptools.rgb32asrgb(rgb32),
                 SCALE,
